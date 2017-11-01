fffac5f4578641f3a72bce3ae549bcd687bda474
hide2 <23771066@qq.com>
2017-11-01 18:06:05 +0800

    wiki

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
 <div class="cl-mcont">
-    show
+
+    <div class="row">
+        <div class="col-sm-7 col-md-7" style="margin-left:21%">
+            <div class="block-flat">
+                <div class="header">                            
+                    <h3>{{ $title or config('app.name', 'CleanWiki') }}</h3>
+                </div>
+                @for ($i = 0; $i < 10; $i++)
+                <div class="content">
+                    <div class="spacer spacer-bottom">
+                        <button type="button" class="btn btn-default btn-rad">Default</button>
+                        <button type="button" class="btn btn-primary btn-rad">Primary</button>
+                        <button type="button" class="btn btn-success btn-rad">Success</button>
+                        <button type="button" class="btn btn-info btn-rad">Info</button>
+                        <button type="button" class="btn btn-warning btn-rad">Warning</button>
+                        <button type="button" class="btn btn-danger btn-rad">Danger</button>
+                    </div>
+                </div>
+                @endfor
+            </div>
+        </div>
+    </div>
+
 </div>
 @endsection
