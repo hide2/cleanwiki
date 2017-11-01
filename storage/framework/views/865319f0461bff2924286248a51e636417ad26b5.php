f3fa8c5015a8ac609ef83c89247e6db7b70eb789
hide2 <23771066@qq.com>
2017-11-01 17:17:31 +0800

    ignore

--- storage/framework/views/865319f0461bff2924286248a51e636417ad26b5.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<div class="cl-mcont">
-    hehe
-</div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
