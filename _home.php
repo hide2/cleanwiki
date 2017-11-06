03349512924b79bec51e92870a1612dae5f29d59
hide2 <23771066@qq.com>
2017-11-06 21:59:54 +0800

    wiki

--- _home.php
+++ _home.php
@@ -12,16 +12,23 @@ require __DIR__.'/_header.php';
 $dir=__DIR__.'/content';
 $file=scandir($dir);
 $md = '';
-foreach ($file as $f) {
-    if (preg_match('/\.php/', $f)) {
-        $f = explode('.php', $f)[0];
-        if ($encod !== 'UTF-8') {
-            $f = iconv('gbk', 'utf-8', $f);
-        }
-        $f = trim($f);
-        $md = $md."- [".$f."](http://".$_SERVER["HTTP_HOST"]."/".$f.")\r\n";
-    }
-}
+// foreach ($file as $f) {
+//     if (preg_match('/\.php/', $f)) {
+//         $f = explode('.php', $f)[0];
+//         if ($encod !== 'UTF-8') {
+//             $f = iconv('gbk', 'utf-8', $f);
+//         }
+//         $f = trim($f);
+//         $md = $md."- [".$f."](http://".$_SERVER["HTTP_HOST"]."/".$f.")\r\n";
+//     }
+// }
+$md = <<<SSS
+- 技术资料
+    - [Demo](/Demo)
+    - [PHP-Laravel](/PHP-Laravel)
+- 读书笔记
+    - [重新定义公司](/重新定义公司)
+SSS;
 echo $Parsedown->text($md);
 
 require __DIR__.'/_footer.php';
