d9fbe1cccee12d6ef14ce384b9d6532d8fb833a5
hide2 <hideto.bj@gmail.com>
2017-11-20 23:59:10 +0800

    wiki

--- content/How-to-Start-a-Startup.php
+++ content/How-to-Start-a-Startup.php
@@ -24,8 +24,14 @@ You'll need passion to effectively recruit
 - `Aptitude`: The world needs you to do it
     - The world needs it
 Otherwise do something that the world needs
+    - The world needs you
+It's bad for the world if you just outcompete a team with more aptitude
+The world needs you somewhere - find where
 
   
+>创业只有一个原因：你不得不去创业，因为命运选择了你
+你拥有要去做这件事的难以抑制的激情+这个世界需要这个东西+你拥有做这件事的最好的天赋
+
 =blue 2. Ideas, Products, Teams and Execution - Sam Altman
 -------------------------
 =blue Ideas
@@ -102,17 +108,62 @@ Set an operation rhythm
 - Launching new features
 - Reviewing/reporting metrics and milestones
 
+The competitor to be feared is one who never bothers about you at all, but goes on making his own business better all the time.
 Don't spend more money than you have
 
-  
+>尽量找熟知的合伙人
+从同行里找到优秀的合伙人
+最好有两三个合伙人
+员工越多不一定越好
+创业初期尽量少招人，减少成本
+为自己的公司找最优秀的人才
+最好找与你一样相信公司，并符合你期待的人
+要熟悉每个你雇佣的人
+要以期权的形式分配股份
+股份的分配最好在公司建立之初就和合伙人协商好
+在执行方面，首先要清楚自己要做什么，其次就是是否能完成
+如果你知道自己要做什么，剩下的就是专注和保持增长势头
+要在许多事情中选出自己觉得最重要的做，而不是漫无目的
+要让整个公司都清楚目标
+保持增长势头可以鼓舞士气，保持信心，一切问题都迎刃而解
+无视你的竞争者，不要让他们破坏你的增长势头
+
 =blue 3. Counterintuitive Parts of Startups, and How to Have Ideas - Paul Graham
 -------------------------
 
+>创业中反直觉的几条原则
+不能一味跟着直觉走
+成功创业需要的并不是创业知识专家
+找诀窍在创业中是行不通的
+创业比想象中的还要艰辛
+创业前途未卜
+获得创意的方法就是不去刻意想创意
+
 =blue 4. Building Product, Talking to Users, and Growing - Adora Cheung
 -------------------------
+>选择行业，并对该行业有所了解
+简单定义企业特点，明确客户群体，实地营销
+关注客户价值
+建立良好的宣传和推荐机制
+三种增长模式 - 高粘度增长、病毒式增长、付费增长
+要坚持创意同时也要在适当的时机学会放弃，找到适合自己的创业路子
+
+在3年里pivot了12次，终于获得$37M融资把Homejoy做起来
+师兄的今日头条也是一个积极pivot并走向成功的例子
+另一个案例是一个同学做微商，一直没有增长，半死不活的状态坚持了很多年，其实他早该pivot
+现实中大多数创业团队都需要转型，而且关键在于转型的速度和质量
+
 
 =blue 5. Business Strategy and Monopoly Theory - Peter Thiel
 -------------------------
+`Competition is for losers`
+- capturing value
+    - X*Y%
+- lies people tell
+- how to build a monopoly
+- last mover advantage
+- history of innovation
+- psychology of competition
 
 =blue 6. Growth - Alex Schultz
 -------------------------
